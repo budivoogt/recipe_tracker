f3af396c31bd55ffcdb6748039c777a723525b0e
brucey0x <11062155+brucey0x@users.noreply.github.com>
2023-10-17 22:13:28 +0200

    working on integrating Supabase server-side authentication. First time using this, had only done client-side with row level security previously.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,21 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import { Session, SupabaseClient } from "@supabase/supabase-js"
+import { Database } from "./types/supabase"
+
 declare global {
 	namespace App {
+		interface Locals {
+			supabase: SupabaseClient<Database>
+			getSession(): Promise<Session | null>
+		}
 		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
+		interface PageData {
+			session: Session | null
+		}
 		// interface Platform {}
 	}
 }
 
-export {};
+export {}
--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js"
 import type { Database } from "../types/supabase"
 
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL
-
-const supabaseAnonKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey)

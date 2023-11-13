eca42d57b2d8114de32a496ef94d57e6af86aff4
brucey0x <11062155+brucey0x@users.noreply.github.com>
2023-11-13 20:57:19 +0100

    added ability to update acquired status of an ingredient in the recipe detail modal.

--- src/lib/components/RecipeDetailsModal.svelte
+++ src/lib/components/RecipeDetailsModal.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
 	import type { SupabaseClient } from "@supabase/supabase-js"
 	import { Checkbox, Modal } from "flowbite-svelte"
+	import { updateRecipe } from "../../stores/recipeStore"
 
     export let showRecipeDetails: boolean
     export let supabase: SupabaseClient
     export let selectedRecipe: Recipe | null
-
     
+    function updateHandler () {
+        if(selectedRecipe) {
+            updateRecipe(supabase, selectedRecipe)
+        } else {
+            console.log("selectedRecipe is null")
+        }
+    }
 </script>
 
 <Modal bind:open={showRecipeDetails} class="min-w-full" outsideclose>
-    <div class="">
+    <div class="border-b-2 border-slate-300 pb-2">
         <h1 class="text-xl font-bold text-gray-700">{selectedRecipe?.name}</h1>
     </div>
     <dl class="grid grid-cols-3 gap-4 text-gray-700">
@@ -21,15 +28,15 @@
         <dt class="font-semibold mt-4">Rating</dt>
         <dd class="row-start-2">{selectedRecipe?.rating || "None"}</dd>
     </dl>
-    <dl class="grid grid-cols-1 gap-4 text-gray-700">
+    <dl class="grid grid-cols-1 gap-4 text-gray-700 ">
         <!-- Discover how to make these dd fields rich text -->
-        <dt class="font-semibold mt-4">Description</dt>
+        <dt class="font-semibold mt-4 border-t-2 border-slate-300 pt-4">Description</dt>
         <dd class="">{selectedRecipe?.description}</dd>
         <dt class="font-semibold mt-4">Instructions</dt>
         <dd class="">{selectedRecipe?.instructions}</dd>
     </dl>
     <div class="">
-        <h1 class="font-semibold text-gray-700 mt-4">Ingredients</h1>
+        <h1 class="font-semibold text-gray-700 mt-4 border-t-2 border-slate-300 pt-4">Ingredients</h1>
     </div>
     <dl class="grid grid-cols-3 gap-4 text-gray-700">
         <dt class="font-semibold">Item</dt>
@@ -38,7 +45,7 @@
         {#each selectedRecipe?.ingredients as ingredient}
             <dd class="">{ingredient.item}</dd>
             <dd class="">{ingredient.quantity}</dd>
-            <Checkbox checked={ingredient.acquired} class=""/>
+            <Checkbox bind:checked={ingredient.acquired} on:change={updateHandler}/>
         {/each}
     </dl>
     

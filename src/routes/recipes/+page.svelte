bdeb43a9edcb154f731ac128f33c7e88d4bc4d98
brucey0x <11062155+brucey0x@users.noreply.github.com>
2023-12-11 17:48:22 +0100

    restored /routes/recipes/+page.svelte

---
+++ src/routes/recipes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import RecipeTable from "$lib/components/RecipeTable.svelte"
+</script>
+
+<svelte:head>
+	<title>Browse Recipes</title>
+	<meta name="description" content="Explore, create and manage your recipes." />
+</svelte:head>
+
+<RecipeTable />
